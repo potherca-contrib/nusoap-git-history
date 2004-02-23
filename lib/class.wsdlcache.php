536f0d72e136e9c76be5f153e8c99bf6ee49cc63
snichol <>
2004-02-23 19:23:17 +0000

    - wsdlcache: synchronize using a per-WSDL lock file (Thanks Ingo) - wsdlcache: add cache lifetime, after which cache contents are invalidated   (Thanks Ingo)

--- lib/class.wsdlcache.php
+++ lib/class.wsdlcache.php
@@ -5,28 +5,28 @@
 /**
 * caches instances of the wsdl class
 * 
-*	Because of limitations of flock, specifically that it is called with
-*	a file handle, mutual exclusion is not implemented by locking the
-*	file containing serialized WSDL.  Further, because semaphores and
-*	alternative IPC mechanisms are not available in PHP on all platforms
-*	or all builds, mutual exclusion is not based on them, either.
-*	Instead, a single lock file is used by all instances of this class
-*	for all WSDL URLs.  This could create scalability issues for applications
-*	with many NuSOAP clients or servers.
-*
 * @author   Scott Nichol <snichol@computer.org>
+* @author	Ingo Fischer <ingo@apollon.de>
 * @version  $Id$
 * @access public 
 */
 class wsdlcache {
 	var $fplock;
+	var $cache_lifetime;
+	var $cache_dir;
+	var $debug_str = '';
 
 	/*
-	* default constructor
-    *
-    * @access   public
-    */
-	function wsdlcache() {
+	* constructor
+	*
+	* @param string $cache_dir directory for cache-files
+	* @param integer $cache_lifetime lifetime for caching-files in seconds or 0 for unlimited
+	* @access public
+	*/
+	function wsdlcache($cache_dir='.', $cache_lifetime=0) {
+		$this->fplock = array();
+		$this->cache_dir = $cache_dir != '' ? $cache_dir : '.';
+		$this->cache_lifetime = $cache_lifetime;
 	}
 
 	/**
@@ -37,7 +37,17 @@ class wsdlcache {
 	* @access protected
 	*/
 	function createFilename($wsdl) {
-		return 'wsdlcache-' . md5($wsdl);
+		return $this->cache_dir.'/wsdlcache-' . md5($wsdl);
+	}
+
+	/**
+	* adds debug data to the class level debug string
+	*
+	* @param    string $string debug data
+	* @access   private
+	*/
+	function debug($string){
+		$this->debug_str .= get_class($this).": $string\n";
 	}
 
 	/**
@@ -49,52 +59,84 @@ class wsdlcache {
 	*/
 	function get($wsdl) {
 		$filename = $this->createFilename($wsdl);
-		$this->obtainMutex();
-		$fp = @fopen($filename, "r");
-		if ($fp) {
-			$s = implode("", @file($filename));
-			fclose($fp);
-		} else {
-			$s = null;
+		if ($this->obtainMutex($filename, "r")) {
+			// check for expired WSDL that must be removed from the cache
+ 			if ($this->cache_lifetime > 0) {
+				if (file_exists($filename) && (time() - filemtime($filename) > $this->cache_lifetime)) {
+					unlink($filename);
+					$this->debug("Expired $wsdl ($filename) from cache");
+  				}
+			}
+			// see what there is to return
+			$fp = @fopen($filename, "r");
+			if ($fp) {
+				$s = implode("", @file($filename));
+				fclose($fp);
+				$this->debug("Got $wsdl ($filename) from cache");
+			} else {
+				$s = null;
+				$this->debug("$wsdl ($filename) not in cache");
+			}
+			$this->releaseMutex($filename);
+			return (!is_null($s)) ? unserialize($s) : null;
 		}
-		$this->releaseMutex();
-		return (!is_null($s)) ? unserialize($s) : null;
+		return null;
 	}
 
 	/**
 	* obtains the local mutex
 	*
+	* @param string $filename The Filename of the Cache to lock
+	* @param string $mode The open-mode ("r" or "w") or the file - affects lock-mode
+	* @return boolean Lock successfully obtained ?!
 	* @access protected
 	*/
-	function obtainMutex() {
-		$this->fplock = fopen("wsdlcache-lock", "w");
-		flock($this->fplock, LOCK_EX);
+	function obtainMutex($filename, $mode) {
+		if (isset($this->fplock[md5($filename)])) {
+			$this->debug("Lock for $filename already exists");
+			return false;
+		}
+		$this->fplock[md5($filename)] = fopen($filename.".lock", "w");
+		if ($mode == "r") {
+			return flock($this->fplock[md5($filename)], LOCK_SH);
+		} else {
+			return flock($this->fplock[md5($filename)], LOCK_EX);
+		}
 	}
 
 	/*
 	* adds a wsdl instance to the cache
 	*
 	* @param object $wsdl_instance The wsdl instance to add
+	* @return boolean WSDL successfully cached
 	* @access public
 	*/
 	function put($wsdl_instance) {
 		$filename = $this->createFilename($wsdl_instance->wsdl);
 		$s = serialize($wsdl_instance);
-		$this->obtainMutex();
-		$fp = fopen($filename, "w");
-		fputs($fp, $s);
-		fclose($fp);
-		$this->releaseMutex();
+		if ($this->obtainMutex($filename, "w")) {
+			$fp = fopen($filename, "w");
+			fputs($fp, $s);
+			fclose($fp);
+			$this->debug("Put $wsdl_instance->wsdl ($filename) in cache");
+			$this->releaseMutex($filename);
+			return true;
+		}
+		return false;
 	}
 
 	/**
 	* releases the local mutex
 	*
+	* @param string $filename The Filename of the Cache to lock
+	* @return boolean Lock successfully released
 	* @access protected
 	*/
-	function releaseMutex() {
-		flock($this->fplock, LOCK_UN);
-		fclose($this->fplock);
+	function releaseMutex($filename) {
+		$ret = flock($this->fplock[md5($filename)], LOCK_UN);
+		fclose($this->fplock[md5($filename)]);
+		unset($this->fplock[md5($filename)]);
+		return $ret;
 	}
 
 	/**
@@ -106,9 +148,10 @@ class wsdlcache {
 	*/
 	function remove($wsdl) {
 		$filename = $this->createFilename($wsdl);
-		$this->obtainMutex();
+		$this->obtainMutex($filename, "w");
 		$ret = unlink($filename);
-		$this->releaseMutex();
+		$this->debug("Removed $wsdl ($filename) from cache");
+		$this->releaseMutex($filename);
 		return $ret;
 	}
 }
