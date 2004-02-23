93005fe0e04e28bc8d4a2cba7d7f542f0b43dd39
snichol <>
2004-02-23 19:22:19 +0000

    Add method calls and wsdl cache debug.

--- samples/wsdlclient5.php
+++ samples/wsdlclient5.php
@@ -16,7 +16,7 @@ $proxyport = isset($_POST['proxyport']) ? $_POST['proxyport'] : '';
 $proxyusername = isset($_POST['proxyusername']) ? $_POST['proxyusername'] : '';
 $proxypassword = isset($_POST['proxypassword']) ? $_POST['proxypassword'] : '';
 
-$cache = new wsdlcache();
+$cache = new wsdlcache('.', 60);
 $wsdl = $cache->get('http://www.xmethods.net/sd/2001/BNQuoteService.wsdl');
 if (is_null($wsdl)) {
 	$wsdl = new wsdl('http://www.xmethods.net/sd/2001/BNQuoteService.wsdl',
@@ -54,5 +54,6 @@ if ($client->fault) {
 }
 echo '<h2>Request</h2><pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
 echo '<h2>Response</h2><pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
+echo '<h2>Cache Debug</h2><pre>' . htmlspecialchars($cache->debug_str, ENT_QUOTES) . '</pre>';
 echo '<h2>Debug</h2><pre>' . htmlspecialchars($client->debug_str, ENT_QUOTES) . '</pre>';
 ?>
