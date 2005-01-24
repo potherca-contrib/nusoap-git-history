1d1bdbf67a73d320769cbff3401bee156d2227b8
snichol <>
2005-01-24 21:58:48 +0000

    Choose from three Amazon calls.

--- samples/wsdlclient12.php
+++ samples/wsdlclient12.php
@@ -5,16 +5,34 @@
  *	WSDL client sample.
  *
  *	Service: WSDL
- *	Payload: rpc/encoded
+ *	Payload: document/literal
  *	Transport: http
  *	Authentication: none
  */
 require_once('../lib/nusoap.php');
+require_once('../lib/class.wsdlcache.php');
 $proxyhost = isset($_POST['proxyhost']) ? $_POST['proxyhost'] : '';
 $proxyport = isset($_POST['proxyport']) ? $_POST['proxyport'] : '';
 $proxyusername = isset($_POST['proxyusername']) ? $_POST['proxyusername'] : '';
 $proxypassword = isset($_POST['proxypassword']) ? $_POST['proxypassword'] : '';
-$client = new soapclient("http://webservices.amazon.com/AWSECommerceService/AWSECommerceService.wsdl", true,
+
+$method = isset($_GET['method']) ? $_GET['method'] : 'ItemSearch';
+
+echo 'You must set your own Amazon E-Commerce Services subscription id in the source code to run this client!'; exit();
+$SubscriptionId = 'Your AWS subscription id';
+
+$wsdlurl = 'http://webservices.amazon.com/AWSECommerceService/US/AWSECommerceService.wsdl';
+$cache = new wsdlcache('.', 120);
+$wsdl = $cache->get($wsdlurl);
+if (is_null($wsdl)) {
+	$wsdl = new wsdl($wsdlurl,
+					$proxyhost, $proxyport, $proxyusername, $proxypassword);
+	$cache->put($wsdl);
+} else {
+	$wsdl->debug_str = '';
+	$wsdl->debug('Retrieved from cache');
+}
+$client = new soapclient($wsdl, true,
 						$proxyhost, $proxyport, $proxyusername, $proxypassword);
 $err = $client->getError();
 if ($err) {
@@ -23,25 +41,81 @@ if ($err) {
 
 $client->soap_defencoding = 'UTF-8';
 
-$itemSearchRequest = array(
-	'BrowseNode' => '53',
-	'ItemPage' => 1,
-//	'ResponseGroup' => array('Request', 'Small'),
-	'SearchIndex' => 'Books',
-	'Sort' => 'salesrank'
-);
+function GetCartCreateParams() {
+	global $SubscriptionId;
 
-echo 'You must set your own Amazon E-Commerce Services subscription id in the source code to run this client!'; exit();
-$itemSearch = array(
-	'SubscriptionId' => 'Your AWS subscription id',
-//	'AssociateTag' => '',
-//	'Validate' => '',
-//	'XMLEscaping' => '',
-//	'Shared' => $itemSearchRequest,
-	'Request' => array($itemSearchRequest)
-);
+	// create items to be added to the cart
+	$item = array ();
+	$item[0] = array(  "ASIN" => "0596004206",
+					   "Quantity" => "1"
+					);
+	$item[1] = array(  "ASIN" => "0596003277",
+					   "Quantity" => "2"
+					);
 
-$result = $client->call('ItemSearch', array('body' => $itemSearch));
+	// pack it to <Item> array
+	$items =  array("Item" => $item);
+	// Construct request parameters
+	$request = array("Items" => $items, "ResponseGroup" => "CartSimilarities");
+	
+	// Construct  all parameters
+	$cartCreate = array(	"SubscriptionId"  => $SubscriptionId,
+							"Request" => $request
+					 	);
+
+	return $cartCreate;
+}
+
+function GetItemSearchParams() {
+	global $SubscriptionId;
+
+	$itemSearchRequest = array(
+		'BrowseNode' => '53',
+		'ItemPage' => 1,
+	//	'ResponseGroup' => array('Request', 'Small'),
+		'SearchIndex' => 'Books',
+		'Sort' => 'salesrank'
+	);
+	
+	$itemSearch = array(
+		'SubscriptionId' => $SubscriptionId,
+	//	'AssociateTag' => '',
+	//	'Validate' => '',
+	//	'XMLEscaping' => '',
+	//	'Shared' => $itemSearchRequest,
+		'Request' => array($itemSearchRequest)
+	);
+	
+	return $itemSearch;
+}
+
+function GetItemSearchParams2() {
+	global $SubscriptionId;
+
+	$request = array(
+		"Keywords" => "postal stamps",
+		"SearchIndex" => "Books"
+	);
+
+	$itemSearch = array(
+		'SubscriptionId' => $SubscriptionId,
+		'Request' => $request
+	);
+
+	return $itemSearch;
+}
+
+if ($method == 'ItemSearch') {
+	$result = $client->call('ItemSearch', array('body' => GetItemSearchParams()));
+} elseif ($method == 'ItemSearch2') {
+	$result = $client->call('ItemSearch', array('body' => GetItemSearchParams2()));
+} elseif ($method == 'CartCreate') {
+	$result = $client->call('CartCreate', array('body' => GetCartCreateParams()));
+} else {
+	echo "Unsupported method $method";
+	exit;
+}
+
 // Check for a fault
 if ($client->fault) {
 	echo '<h2>Fault</h2><pre>';
